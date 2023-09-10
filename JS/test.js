c2560564fd52305347e6f9e483154239362eb712
mairohanhoon <rohantiwari2709@gmail.com>
2023-09-10 10:25:15 +0530

    new files added

---
+++ JS/test.js
@@ -0,0 +1,14 @@
+
+const accountId = 121215
+let accountEmail = "rohan@gmail.com"
+var accountPassword = "123@asd"
+accountCity = "Dehradun"
+let accountState
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+accountEmail = "acs@yahoo.com"
+accountPassword = "6969"
+accountCity = "Delhi"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
