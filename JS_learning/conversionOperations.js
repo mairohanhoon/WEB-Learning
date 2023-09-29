cf0d957b73dd234ea806e5be7f848f1791ea253f
mairohanhoon <rohantiwari2709@gmail.com>
2023-09-29 20:56:42 +0530

    changes made in new branch

---
+++ JS_learning/conversionOperations.js
@@ -0,0 +1,8 @@
+let score = "33abc";
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+// **********Operations**********
+
---
+++ JS_learning/stringLearning.js
@@ -0,0 +1,7 @@
+const name = "Rohan"
+const repoCount = 5
+
+console.log(name + repoCount + "Value"); // old and bad practice
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+// new and better readable code
