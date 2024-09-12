0c8752e5df9b902ca95a57514d0ab86c09fb11ed
mairohanhoon <rohantiwari2709@gmail.com>
2024-09-12 21:52:53 +0530

    added new files

---
+++ React/12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,76 @@
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteURL)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if(userAccount){
+                // call another method 
+                return this.login({email, password});
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try{
+            return await this.account.createEmailSession(email, password);
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            return await this.account.get();
+        }
+        catch(error){
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+            
+        }
+        return null;
+    }
+
+    async logout(){
+        try{
+            await this.deleteSessions();
+        }
+        catch(error){
+            console.log("Appwrite service :: logout :: error", error);
+            
+        }
+    }
+} 
+
+const authService = new AuthService();
+
+export default authService;
+
+// const client = new Client()
+//     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//     .setProject('<PROJECT_ID>');                 // Your project ID
+
+// const account = new Account(client);
+
+// const user = await account.create(
+//     ID.unique(), 
+//     'email@example.com', 
+//     'password'
+// );
